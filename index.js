790c199df78e811bc8ebed34243312301334191c
Matt K <matthedud@hotmail.com>
2022-03-17 14:54:06 +0100

    finish

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb+srv://matthedud:FSBN5YY7U4DUv1qh@cluster0.yyotl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,9 +15,27 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+  .then( async () => {
+    await Recipe.create({
+      title: "Poisson",
+      level: "Difficile",
+      ingredients: ["Thon", "oeuf", "mayo"],
+      cuisine: "Rouge",
+      dishType: "Bole",
+      image: "https://www.oceano.org/wp-content/uploads/2020/09/Thon-blanc-Collections-de-lInstitut-oceanographique-Michel-Dagnino-768x401.jpg",
+      duration: 3,
+      creator: "Aquaman",
+      created: new Date(),
+    })
+    const savedRecepies = await Recipe.insertMany(data)
+    savedRecepies.forEach(recepi=>console.log('title', recepi.title))
+    await Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"}, {duration:100})
+    await Recipe.deleteOne({title:"Carrot Cake"})
+    mongoose.connection.close()
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  
