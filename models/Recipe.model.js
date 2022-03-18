797c1b597857c4a8eff8b008dd663151b6ffd79d
Matt K <matthedud@hotmail.com>
2022-03-18 14:39:49 +0100

    schema Adon

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,18 +1,33 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 
 const recipeSchema = new Schema({
-  title: String,
-  level: String,
+  title: { type: String, required: true },
+  level: {
+    type: String,
+    validate: (value) => value === "Easy Peasy" || value === "Amateur" || value === "UltraPro Chef",
+    message: `C'est pas bien`
+  },
   ingredients: [String],
-  cuisine: String,
-  dishType: String,
-  image: String,
-  duration: Number,
+  cuisine: { type: String, required: true },
+  dishType: {
+    type: String,
+    validate: (value) =>
+      value === "breakfast" ||
+      value === "main_course" ||
+      value === "soup" ||
+      value === "snack" ||
+      value === "drink" ||
+      value === "dessert" ||
+      value === "other",
+      message: `C'est pas bien`
+    },
+  image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg" },
+  duration: { type: Number, min: 0 },
   creator: String,
-  created: Date,
-});
+  created: { type: Date, default: new Date() },
+})
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema)
 
-module.exports = Recipe;
+module.exports = Recipe
